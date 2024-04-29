87084badadf22b5c566fdd9fb45b96b8f181c9c1
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-04-29 23:17:34 +0700

    shrink font size

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -9,7 +9,7 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        guifont = jit.os == "OSX" and "MonaspiceNe Nerd Font:h17" or "Iosevka SS17,Iosevka Nerd Font:h12",
+        guifont = jit.os == "OSX" and "MonaspiceNe Nerd Font:h16" or "Iosevka SS17,Iosevka Nerd Font:h12",
       },
       g = { -- vim.g.<key>
         catppuccin_flavour = "mocha",
