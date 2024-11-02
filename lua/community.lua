be2f87f950c01ce78a814af68c35c59cf17f649d
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-11-02 18:01:31 +0700

    remove dart community plugin & update deps

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
-  { import = "astrocommunity.pack.dart" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- import/override with your plugins folder
