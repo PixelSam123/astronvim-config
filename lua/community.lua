b2eff3384e83af8e1ff8373d057730f947d79e90
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-11-18 10:34:54 +0700

    reorder community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -6,9 +6,9 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
-  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
   -- import/override with your plugins folder
 }
